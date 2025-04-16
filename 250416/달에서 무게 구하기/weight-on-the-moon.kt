948e499e015c8a6afe2d8073cbd8c306403d7342
khy1121 <rlagjsdud3@hansung.ac.kr>
2025-04-16 15:43:10 +0900

    Title: 달에서 무게 구하기, Time: 113ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 250416/달에서 무게 구하기/weight-on-the-moon.kt
@@ -0,0 +1,10 @@
+fun main(){
+    var a= 13
+    var b :Double= 0.165
+    var B= String.format("%.6f", b)
+    var d :Double = a*b
+    var D = String.format("%.6f", d)
+    print("$a * $B = $D")
+
+}
+
