7a194ed658ad7eb5098f2d8035faec3a80d434a2
khy1121 <rlagjsdud3@hansung.ac.kr>
2025-04-20 21:08:22 +0900

    Title: 세 실수의 반올림, Time: 105ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250420/세 실수의 반올림/rounding-of-three-actual-numbers.kt
@@ -0,0 +1,13 @@
+
+fun main(args:Array<String>){
+    var a = readln()
+    var b = readln()
+    var c = readln()
+
+    var x =a.toString().toDouble()
+    var y =b.toString().toDouble()
+    var z =c.toString().toDouble()
+    println(String.format("%.3f",x))
+    println(String.format("%.3f", y))
+    println(String.format("%.3f", z ))
+}
